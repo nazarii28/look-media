a0f38271602ddacb2fd8cd3f3a7ad75f50da6362
nazarii28 <nizirko@gmail.com>
2022-07-19 21:54:38 +0200

    changed router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 import AppRoutes from "./components/AppRoutes";
 import {Provider} from "react-redux";
 import store from './store'
